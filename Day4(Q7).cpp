d0335dbb68a0b4d796020ad0ae056e6ac0d1986a
Vishnu <vishnu2007as@gmail.com>
2025-11-13 18:24:12 +0530

    Add files via upload

---
+++ Day4(Q7).cpp
@@ -0,0 +1,18 @@
+//Write a program to swap two numbers without using a third variable.
+
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    printf("Before swap: x=%d y=%d\n", x, y);
+
+    x = x + y;
+    y = x - y;
+    x = x - y;
+
+    printf("After swap: x=%d y=%d\n", x, y);
+    return 0;
+}
