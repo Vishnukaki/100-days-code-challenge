4def2516a8d3c93eaef890f868d87793ba9ef578
Vishnu <vishnu2007as@gmail.com>
2025-11-13 15:36:34 +0530

    Refactor input and output for addition program

--- Day 1 (Q1).c
+++ Day 1 (Q1).c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int a, b, total;
-    printf("Please enter two integers: ");
-    scanf("%d %d", &a, &b);
-    total = a + b;
-    printf("The total is: %d\n", total);
-    return 0;
+    int num1, num2, sum;   
+    
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+    
+    sum = num1 + num2;
+
+    printf("Sum of %d and %d is %d\n", num1, num2, sum);
+
+    return 0;  
 }
