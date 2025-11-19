d08431dafd705895b6494ba4a32c3326acb56539
Vishnu <vishnu2007as@gmail.com>
2025-11-19 23:17:34 +0530

    Add files via upload

---
+++ Day20(Q40).cpp
@@ -0,0 +1,28 @@
+// Write a program to find the 1’s complement of a binary number and print it.
+
+
+#include <stdio.h>
+
+int main() {
+    char bin[100];
+    int i;
+
+    printf("Enter a binary number: ");
+    scanf("%s", bin);
+
+ 
+    for (i = 0; bin[i] != '\0'; i++) {
+        if (bin[i] == '0')
+            bin[i] = '1';
+        else if (bin[i] == '1')
+            bin[i] = '0';
+        else {
+            printf("Invalid binary number.\n");
+            return 0;
+        }
+    }
+
+    printf("1's Complement = %s\n", bin);
+
+    return 0;
+}
