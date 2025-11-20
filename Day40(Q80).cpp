eadb4cc7c61191ac4131497f24a82aa0b6627768
Vishnu <vishnu2007as@gmail.com>
2025-11-20 18:54:47 +0530

    Add files via upload

---
+++ Day40(Q80).cpp
@@ -0,0 +1,54 @@
+// Multiply two matrices.
+
+#include <stdio.h>
+
+int main() {
+    int r1, c1, r2, c2, i, j, k;
+
+    printf("Enter number of rows and columns of first matrix: ");
+    scanf("%d %d", &r1, &c1);
+
+    int matrix1[100][100];
+    printf("Enter elements of first matrix:\n");
+    for (i = 0; i < r1; i++) {
+        for (j = 0; j < c1; j++) {
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    printf("Enter number of rows and columns of second matrix: ");
+    scanf("%d %d", &r2, &c2);
+
+    if (c1 != r2) {
+        printf("Matrix multiplication not possible! Columns of first must equal rows of second.\n");
+        return 0;
+    }
+
+    int matrix2[100][100];
+    printf("Enter elements of second matrix:\n");
+    for (i = 0; i < r2; i++) {
+        for (j = 0; j < c2; j++) {
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+    int product[100][100] = {0};
+
+    for (i = 0; i < r1; i++) {
+        for (j = 0; j < c2; j++) {
+            for (k = 0; k < c1; k++) {
+                product[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
+        }
+    }
+
+    printf("Product of the matrices:\n");
+    for (i = 0; i < r1; i++) {
+        for (j = 0; j < c2; j++) {
+            printf("%d ", product[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
