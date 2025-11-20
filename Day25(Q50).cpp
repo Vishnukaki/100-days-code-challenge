9200f2d46c127e80cf876a61ee9e83c68230d625
Vishnu <vishnu2007as@gmail.com>
2025-11-20 15:28:46 +0530

    Add files via upload

---
+++ Day25(Q50).cpp
@@ -0,0 +1,25 @@
+// Write a program to print the following pattern:
+*****
+ ****
+  ***
+   **
+    *
+
+
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for (i = 0; i < 5; i++) {             
+        for (j = 0; j < i; j++) {         
+            printf(" ");
+        }
+        for (j = 0; j < 5 - i; j++) {    
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
