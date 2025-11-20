78b588870d1ec17bb4dfeaef7e8ca1b61aa85d81
Vishnu <vishnu2007as@gmail.com>
2025-11-20 16:19:34 +0530

    Add files via upload

---
+++ Day26(Q52).cpp
@@ -0,0 +1,42 @@
+// Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+
+
+#include <stdio.h>
+
+int main() {
+
+    // 1 star
+    printf("*\n\n");
+
+    // 3 stars
+    printf("*\n*\n*\n\n");
+
+    // 5 stars
+    printf("*\n*\n*\n*\n*\n\n");
+
+    // 3 stars
+    printf("*\n*\n*\n\n");
+
+    // 1 star
+    printf("*\n");
+
+    return 0;
+}
