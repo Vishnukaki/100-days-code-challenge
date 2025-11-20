7c86bfb2d119402c16a84811497ca1ccd02ed24b
Vishnu <vishnu2007as@gmail.com>
2025-11-20 16:48:51 +0530

    Add files via upload

---
+++ Day35(Q69).cpp
@@ -0,0 +1,45 @@
+// Find the second largest element in an array.
+
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    if (n < 2) {
+        printf("Array must have at least two elements.\n");
+        return 0;
+    }
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int largest = arr[0];
+    int second = -2147483648;  // Smallest possible int
+
+    // Find largest and second largest
+    for (i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            second = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] > second && arr[i] != largest) {
+            second = arr[i];
+        }
+    }
+
+    if (second == -2147483648) {
+        printf("No second largest element (all elements are equal).\n");
+    } else {
+        printf("Second largest element = %d\n", second);
+    }
+
+    return 0;
+}
