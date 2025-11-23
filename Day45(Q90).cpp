060ab0fd1675568b14381c954f7688d0cac89bfd
Vishnu <vishnu2007as@gmail.com>
2025-11-23 19:35:49 +0530

    Add files via upload

---
+++ Day45(Q90).cpp
@@ -0,0 +1,27 @@
+// Toggle case of each character in a string.
+
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++) {
+
+        // Convert lowercase ? uppercase
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 32;
+        }
+        // Convert uppercase ? lowercase
+        else if (str[i] >= 'A' && str[i] <= 'Z') {
+            str[i] = str[i] + 32;
+        }
+    }
+
+    printf("Toggled string: %s", str);
+
+    return 0;
+}
