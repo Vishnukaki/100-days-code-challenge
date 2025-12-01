21cf7644b30b7db7adf3362a98342b920901f8fb
Vishnu <vishnu2007as@gmail.com>
2025-12-02 01:43:41 +0530

    Update and rename Day20(Q40).cpp to  Q31 - Q40/Day20(Q40).cpp

--- Q31 - Q40/Day20(Q40).cpp
+++ Q31 - Q40/Day20(Q40).cpp
@@ -1,4 +1,4 @@
-// Write a program to find the 1’s complement of a binary number and print it.
+// Write a program to find the 1â€™s complement of a binary number and print it.
 
 
 #include <stdio.h>
